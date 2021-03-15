7b2780a1d4f3f6e8120fbbfa4c2d1e77671495aa
Aaditya Shah <aadityashah02@gmail.com>
2021-03-15 11:30:48 -0500

    Initial Commit

---
+++ Slots Game/Slots_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_GameApp.swift
+//  Slots Game
+//
+//  Created by Aaditya Shah on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Slots_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
